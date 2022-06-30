c1c94923eb9e4b401a85a839b45a39d0a1b016e5
Ubuntu <ubuntu@ip-172-31-23-140.us-east-2.compute.internal>
2022-06-30 23:13:45 +0000

    first commit

---
+++ models/marts/engineering/pull_requests.sql
@@ -0,0 +1,29 @@
+select
+    pull_requests.pull_request_id,
+    repositories.name as repo_name,
+    issues.number as pull_request_number,
+
+    -- TODO: find out how to label these PRs
+    cast(null as string) as type, -- (bug, eng, feature),
+
+    case
+        when pull_requests.is_draft then 'draft'
+        when issues_merged.merged_at is not null then 'merged'
+        when issues.closed_at is not null then 'closed_without_merge'
+        else 'open'
+    end as state,
+
+    issues.created_at as opened_at,
+    issues_merged.merged_at,
+    round(date_diff(issues_merged.merged_at, issues.created_at, hour) / 24.0, 2) as days_open_to_merge
+
+from pull_requests
+
+left join repositories
+    on pull_requests.head_repo_id = repositories.repo_id
+
+left join issues
+    on pull_requests.issue_id = issues.issue_id
+
+left join issues_merged
+    on pull_requests.issue_id = issues_merged.issue_id
